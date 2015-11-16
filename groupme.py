c0a7a86684cd49b7678ebcb04d1f38d61feae14f
bstascavage <brian@stascavage.com>
2015-11-16 16:35:12 +0000

    More logic updates and shit

--- groupme.py
+++ groupme.py
@@ -1,6 +1,7 @@
 import json
 import requests 
 import six
+import time
 from six.moves.urllib import parse as urlparse
 
 class Groupme:
@@ -27,6 +28,18 @@ class Groupme:
 		]
             }
 	try:
-	    content = requests.post(self.config.ConfigSectionMap('groupme')['url'], data=json.dumps(message))
+	    retry = 0
+	    while retry < 20:
+	    	content = requests.post(self.config.ConfigSectionMap('groupme')['url'], data=json.dumps(message))
+	    	self.logger.info("Send groupme message: %s" % message)
+	    	self.logger.info("Groupme response: %s" % content.status_code)
+
+		if content.status_code != 202:
+		    self.logger.info("Message not sent to groupme.  Retrying...")
+		    retry += 1
+		    time.sleep(5)
+		else:
+		    retry = 20
+	    
 	except:
 	    self.logger.error("Cannot connect to groupme. The glory of trains will go unheard.")
--- trainlogic.py
+++ trainlogic.py
@@ -16,51 +16,28 @@ class TrainLogic:
 	if message['user_id'] != self.config.ConfigSectionMap('groupme')['bot_user_id']:
 	    if not message['id'] in self.messages:
 	        self.messages.append(message['id'])
-		dictionary = self.config.ConfigSectionMap('bot')['trigger_words'].split()
-
-		sentence = message['text'].split()
-		sentence = [word.encode('utf-8') for word in sentence]
-                sentence = [word.lower() for word in sentence]
-		sentence = [word.replace(".", "") for word in sentence]
-                sentence = [word.replace(",", "") for word in sentence]
-		if set(dictionary).intersection(sentence):
+		if self.checkMessage(message['text']):
+		    responsetextfile = self.config.ConfigSectionMap('bot')['textfile']
+		    f = open('/var/www/trainbot/traintext.txt', "r")
+		    lines = f.readlines()
+		    f.close()
 		    time.sleep(1)
-		    print "Received message: %s" % message
-  		    self.logger.info("Received message: %s" % message)
-	            self.groupme.post_message('HEEEEEERE THEY COME!', 'http://i.groupme.com/350x460.jpeg.91879800f84743648a947d7080d35380.large')
-		    time.sleep(1.2)
-		    self.groupme.post_message('CLICKETY-CLACK')
-                    time.sleep(1.2)
-                    self.groupme.post_message('D')
-                    time.sleep(1.2)
-                    self.groupme.post_message('O')
-                    time.sleep(1.2)
-                    self.groupme.post_message('W')
-                    time.sleep(1.2)
-                    self.groupme.post_message('N')
-                    time.sleep(1.2)
-                    self.groupme.post_message(' ')
-                    time.sleep(1.2)
-                    self.groupme.post_message('T')
-                    time.sleep(1.2)
-                    self.groupme.post_message('H')
-                    time.sleep(1.2)
-                    self.groupme.post_message('E')
-                    time.sleep(1.2)
-                    self.groupme.post_message(' ')
-                    time.sleep(1.2)
-                    self.groupme.post_message('T')
-                    time.sleep(1.2)
-                    self.groupme.post_message('R')
-                    time.sleep(1.2)
-                    self.groupme.post_message('A')
-                    time.sleep(1.2)
-                    self.groupme.post_message('C')
-                    time.sleep(1.2)
-                    self.groupme.post_message('K')
-                    time.sleep(1.2)
-                    self.groupme.post_message('S')
-                    time.sleep(1.2)
-                    self.groupme.post_message('!')
-                    time.sleep(1.2)
-                    self.groupme.post_message("IT'S LOTS & LOTS OF TRAINS!")
+
+		    for line in lines:
+			if 'jpeg' in line:
+		  	    self.groupme.post_message('', line)
+			else:
+			    self.groupme.post_message(line)
+			time.sleep(1.2)
+
+
+    def checkMessage(self, text):
+	dictionary = self.config.ConfigSectionMap('bot')['trigger_words'].split()
+
+	sentence = text.split()
+	sentence = [word.encode('utf-8') for word in sentence]
+        sentence = [word.lower() for word in sentence]
+	sentence = [word.replace(".", "") for word in sentence]
+        sentence = [word.replace(",", "") for word in sentence]
+  	if set(dictionary).intersection(sentence):
+	    return True
